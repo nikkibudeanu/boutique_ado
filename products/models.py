8b00780a34681e366a9613d16fce39d4df0c7d4b
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-07-24 17:10:03 +0000

    add products to db

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
+
 from django.db import models
+
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
@@ -6,7 +8,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
 
     def get_friendly_name(self):
         return self.friendly_name
@@ -22,9 +23,5 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-
     def __str__(self):
         return self.name
-
-
-
